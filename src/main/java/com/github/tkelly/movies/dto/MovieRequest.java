3627b1ff15e03cfd7dfec16cd6d084196177bb55
Tom Kelly <tckelly312@gmail.com>
2025-06-29 22:25:02 -0400

    cleanup validations

--- src/main/java/com/github/tkelly/movies/dto/MovieRequest.java
+++ src/main/java/com/github/tkelly/movies/dto/MovieRequest.java
@@ -1,14 +1,20 @@
 package com.github.tkelly.movies.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public class MovieRequest {
     private long id;
+
     @NotBlank(message = "Title cannot be blank.")
     private String title;
+
     @NotNull(message = "Release year cannot be null.")
-    private int releaseYear;
+    @Min(value = 1888, message = "Release year must be no earlier than 1888")
+    @Max(value = 2100, message = "Release year must be no later than 2100")
+    private Integer releaseYear;
 
     public long getId() {
         return id;
